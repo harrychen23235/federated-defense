c549e3628a131fb658e6a88cbcf6f77268967534
Chen Congwen <58384208+harrychen23235@users.noreply.github.com>
2022-11-19 22:43:13 +0100

    smaller training size

--- federated_learning/src/trigger_location_femnist.sh
+++ federated_learning/src/trigger_location_femnist.sh
@@ -5,8 +5,8 @@ do
     for j in {0..24..5}
     do  
     echo "currently running location $i $j"
-    python src/federated.py --data=fedemnist --load_pretrained=False --local_ep=2 --bs=256 --num_agents=10 --rounds=15 --partition='homo' \
+    python src/federated.py --data=fedemnist --load_pretrained=False --local_ep=2 --bs=256 --num_agents=10 --rounds=10 --partition='homo' \
         --num_corrupt=4  --client_lr=0.1 --poison_lr=0.05 --attack_mode='normal' --poison_frac=0.2 --malicious_style=mixed  \
-        --attack_start_round=10 --storing_dir='./femnist_pattern_location_'$i'_'$j  --pattern_type="location_test" --pattern_location $i $j
+        --attack_start_round=5 --storing_dir='./femnist_pattern_location_'$i'_'$j  --pattern_type="location_test" --pattern_location $i $j
     done
 done
