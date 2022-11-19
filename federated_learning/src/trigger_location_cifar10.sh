64b7c7febb41f95d288b9a9fde48f5b1ff8c01bf
Chen Congwen <58384208+harrychen23235@users.noreply.github.com>
2022-11-19 22:39:14 +0100

    update location

--- federated_learning/src/trigger_location_cifar10.sh
+++ federated_learning/src/trigger_location_cifar10.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 echo "start running trigger location for cifar10"
-for i in {0..28..3}
+for i in {0..28..5}
 do
-    for j in {0..28..3}
+    for j in {0..28..5}
     do  
     echo "currently running location $i $j"
-    python src/federated.py --data=cifar10 --local_ep=2 --bs=256 --num_agents=10 --rounds=20 --partition='homo' \
+    python src/federated.py --data=cifar10 --local_ep=2 --bs=256 --num_agents=10 --rounds=15 --partition='homo' \
             --num_corrupt=4  --client_lr=0.1 --attack_mode='normal' --poison_frac=0.1 --malicious_style=mixed  \
             --attack_start_round=10  --load_pretrained=True --pretrained_path='../data/saved_models/cifar_pretrain/model_last.pt.tar.epoch_200' \
             --storing_dir='./cifar10_pattern_location_'$i'_'$j  --pattern_type="location_test" --pattern_location $i $j
--- federated_learning/src/trigger_location_femnist.sh
+++ federated_learning/src/trigger_location_femnist.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 echo "start running trigger location for femnist"
-for i in {0..24..3}
+for i in {0..24..5}
 do
-    for j in {0..24..3}
+    for j in {0..24..5}
     do  
     echo "currently running location $i $j"
-    python src/federated.py --data=fedemnist --load_pretrained=False --local_ep=2 --bs=256 --num_agents=10 --rounds=20 --partition='homo' \
+    python src/federated.py --data=fedemnist --load_pretrained=False --local_ep=2 --bs=256 --num_agents=10 --rounds=15 --partition='homo' \
         --num_corrupt=4  --client_lr=0.1 --poison_lr=0.05 --attack_mode='normal' --poison_frac=0.2 --malicious_style=mixed  \
         --attack_start_round=10 --storing_dir='./femnist_pattern_location_'$i'_'$j  --pattern_type="location_test" --pattern_location $i $j
     done
