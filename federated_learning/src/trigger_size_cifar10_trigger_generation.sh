d0c873ffdb983b3c4c4854187d6ec541f3567b02
Chen Congwen <58384208+harrychen23235@users.noreply.github.com>
2022-11-24 21:56:15 +0100

    update path

--- federated_learning/src/trigger_size_cifar10_trigger_generation.sh
+++ federated_learning/src/trigger_size_cifar10_trigger_generation.sh
@@ -6,6 +6,6 @@ do
     python src/federated.py --data=cifar10 --local_ep=2 --bs=256 --num_agents=10 --rounds=15 --partition='homo' \
             --num_corrupt=2  --client_lr=0.1 --attack_mode='fixed_generator' --poison_frac=0.1 --malicious_style=mixed  \
             --attack_start_round=5  --load_pretrained=True --pretrained_path='../data/saved_models/cifar_pretrain/model_last.pt.tar.epoch_200' \
-            --storing_dir='./cifar10_trigger_generation_pattern_size_'$i --pattern_size=$i --pattern_type="size_test" \
+            --storing_dir='./trigger_generation/cifar10_trigger_generation_pattern_size_'$i --pattern_size=$i --pattern_type="size_test" \
             --args.alpha=0.5
 done
