846c6ff68615ea1d63b41a06e4e1bc828d24af21
Chen Congwen <58384208+harrychen23235@users.noreply.github.com>
2022-11-27 23:35:08 +0100

    change path

--- federated_learning/src/trigger_size_tinyimagenet.sh
+++ federated_learning/src/trigger_size_tinyimagenet.sh
@@ -5,7 +5,7 @@ do
     echo "currently running trigger size $i"
     python src/federated.py --data=tiny-imagenet --local_ep=2 --bs=256 --num_agents=20 --rounds=10 --partition='homo' \
             --num_corrupt=4  --client_lr=0.1 --attack_mode='normal' --poison_frac=0.1 --malicious_style=mixed  \
-            --attack_start_round=5  --load_pretrained=True --pretrained_path='../data/saved_models/cifar_pretrain/model_last.pt.tar.epoch_200' \
+            --attack_start_round=5  --load_pretrained=True --pretrained_path='../data/saved_models/tiny_64_pretrain/tiny-resnet.epoch_20' \
             --storing_dir='./trigger_size_imagenet/imagenet_pattern_size_'$i --pattern_size=$i --pattern_type="size_test" --save_checkpoint=True \
 
 done
