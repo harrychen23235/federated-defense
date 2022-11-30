a6476b6cd281518de22f4c9d16d37be16dcf7957
Chen Congwen <58384208+harrychen23235@users.noreply.github.com>
2022-11-30 18:16:46 +0100

    bug fix

--- federated_learning/src/trigger_size_tiny_trigger_generation.sh
+++ federated_learning/src/trigger_size_tiny_trigger_generation.sh
@@ -6,5 +6,5 @@ do
     python src/federated.py --data=tiny-imagenet --local_ep=2 --bs=256 --num_agents=20 --rounds=15 --partition='homo' \
             --num_corrupt=4  --client_lr=0.001 --poison_lr=0.001 --attack_mode='fixed_generator' --poison_frac=0.3 --malicious_style=mixed  \
             --attack_start_round=5  --load_pretrained=True --pretrained_path='../data/saved_models/tiny_64_pretrain/tiny-resnet.epoch_20' \
-            --storing_dir='./trigger_generation_share_vector_4_mali/tiny_pattern_size_trigger_generation_'$i --pattern_size=$i --pattern_type="size_test" --save_checkpoint=True \
+            --storing_dir='./trigger_generation_share_vector_4_mali/tiny_pattern_size_trigger_generation_'$i --pattern_size=$i --pattern_type="size_test" --save_checkpoint=True
 done
