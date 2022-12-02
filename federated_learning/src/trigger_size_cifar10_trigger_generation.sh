718dcf2cfef25e58193ef96e224c9a2cf5d64b33
Chen Congwen <58384208+harrychen23235@users.noreply.github.com>
2022-12-02 15:40:31 +0100

    seperate vector bash

--- federated_learning/src/trigger_size_cifar10_trigger_generation.sh
+++ federated_learning/src/trigger_size_cifar10_trigger_generation.sh
@@ -6,6 +6,6 @@ do
     python src/federated.py --data=cifar10 --local_ep=2 --bs=256 --num_agents=20 --rounds=15 --partition='homo' \
             --num_corrupt=4  --client_lr=0.1 --attack_mode='fixed_generator' --poison_frac=0.1 --malicious_style=mixed  \
             --attack_start_round=5  --load_pretrained=True --pretrained_path='../data/saved_models/cifar_pretrain/model_last.pt.tar.epoch_200' \
-            --storing_dir='./trigger_generation_share_vector_4_mali/cifar10_trigger_generation_pattern_size_'$i --pattern_size=$i --pattern_type="size_test" \
-            --alpha=0.8 --save_checkpoint=True
+            --storing_dir='./trigger_generation_seperate_vector_4_mali/cifar10_trigger_generation_pattern_size_'$i --pattern_size=$i --pattern_type="size_test" \
+            --alpha=0.8 --save_checkpoint=True --seperate_vector=True --save_trigger=True
 done
