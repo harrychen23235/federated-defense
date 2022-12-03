e28da34ef6e2cd5e7595eae41278b06b03641446
Chen Congwen <58384208+harrychen23235@users.noreply.github.com>
2022-12-03 20:26:36 +0100

    Update perturbation_cifar10.sh

--- federated_learning/src/perturbation_cifar10.sh
+++ federated_learning/src/perturbation_cifar10.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo "start running perturbation for cifar10"
-for i in {50..10..7}
+for i in {12..2..3}
 do
     echo "currently running norm cap $i"
     python src/federated.py --data=cifar10 --local_ep=2 --bs=256 --num_agents=20 --rounds=15 --partition='homo' \
