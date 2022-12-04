e190c8a85c1152707793f09b2dfd274e786d90d3
Chen Congwen <58384208+harrychen23235@users.noreply.github.com>
2022-12-04 19:31:58 +0100

    update name

--- federated_learning/src/perturbation_tiny.sh
+++ federated_learning/src/perturbation_tiny.sh
@@ -6,6 +6,6 @@ do
     python src/federated.py --data=tiny-imagenet --local_ep=2 --bs=256 --num_agents=20 --rounds=15 --partition='homo' \
             --num_corrupt=4  --client_lr=0.001 --poison_lr=0.001 --attack_mode='fixed_generator' --poison_frac=0.3 --malicious_style=mixed  \
             --attack_start_round=5  --load_pretrained=True --pretrained_path='../data/saved_models/tiny_64_pretrain/tiny-resnet.epoch_20' \
-            --storing_dir='./perturbation_seperate_vector_4_mali/tiny_perturbation_'$i --pattern_type="pixel" --save_checkpoint=True \
+            --storing_dir='./perturbation_seperate_vector_4_mali/tiny_perturbation_div_10'$i --pattern_type="pixel" --save_checkpoint=True \
             --seperate_vector=True --save_trigger=True --norm_cap=$i --generator_lr=0.1
 done
