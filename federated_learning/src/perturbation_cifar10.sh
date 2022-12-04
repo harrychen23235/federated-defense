64b3463bb25c0c03d7246400ee8f93e0f34c3b0c
Chen Congwen <58384208+harrychen23235@users.noreply.github.com>
2022-12-04 19:32:34 +0100

    update name

--- federated_learning/src/perturbation_cifar10.sh
+++ federated_learning/src/perturbation_cifar10.sh
@@ -6,6 +6,6 @@ do
     python src/federated.py --data=cifar10 --local_ep=2 --bs=256 --num_agents=20 --rounds=15 --partition='homo' \
             --num_corrupt=4  --client_lr=0.1 --attack_mode='fixed_generator' --poison_frac=0.1 --malicious_style=mixed  \
             --attack_start_round=5  --load_pretrained=True --pretrained_path='../data/saved_models/cifar_pretrain/model_last.pt.tar.epoch_200' \
-            --storing_dir='./perturbation_seperate_vector_4_mali/cifar10_perturbation_div_10'$i --pattern_type="pixel" \
+            --storing_dir='./perturbation_seperate_vector_4_mali/cifar10_perturbation_div_10_'$i --pattern_type="pixel" \
             --alpha=0.8 --save_checkpoint=True --seperate_vector=True --save_trigger=True --norm_cap=$i --generator_lr=0.1
 done
