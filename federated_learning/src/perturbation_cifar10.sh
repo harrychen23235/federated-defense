9451623f5c4136fecbd1c77ba12976264af8fbc9
Chen Congwen <58384208+harrychen23235@users.noreply.github.com>
2022-12-04 19:54:40 +0100

    less rnd

--- federated_learning/src/perturbation_cifar10.sh
+++ federated_learning/src/perturbation_cifar10.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 echo "start running perturbation for cifar10"
-for i in {2..10..2}
+for i in {2..12..2}
 do
     echo "currently running norm cap $i"
-    python src/federated.py --data=cifar10 --local_ep=2 --bs=256 --num_agents=20 --rounds=15 --partition='homo' \
+    python src/federated.py --data=cifar10 --local_ep=2 --bs=256 --num_agents=20 --rounds=8 --partition='homo' \
             --num_corrupt=4  --client_lr=0.1 --attack_mode='fixed_generator' --poison_frac=0.1 --malicious_style=mixed  \
-            --attack_start_round=5  --load_pretrained=True --pretrained_path='../data/saved_models/cifar_pretrain/model_last.pt.tar.epoch_200' \
+            --attack_start_round=3  --load_pretrained=True --pretrained_path='../data/saved_models/cifar_pretrain/model_last.pt.tar.epoch_200' \
             --storing_dir='./perturbation_init_norm_seperate_vector_4_mali/cifar10_perturbation_'$i --pattern_type="pixel" \
             --alpha=0.8 --save_checkpoint=True --seperate_vector=True --save_trigger=True --norm_cap=$i --generator_lr=0.1
 done
--- federated_learning/src/perturbation_tiny.sh
+++ federated_learning/src/perturbation_tiny.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 echo "start running perturbation for tiny-imagenet"
-for i in {2..10..2}
+for i in {2..12..2}
 do
     echo "currently running norm cap $i"
-    python src/federated.py --data=tiny-imagenet --local_ep=2 --bs=256 --num_agents=20 --rounds=15 --partition='homo' \
+    python src/federated.py --data=tiny-imagenet --local_ep=2 --bs=256 --num_agents=20 --rounds=8 --partition='homo' \
             --num_corrupt=4  --client_lr=0.001 --poison_lr=0.001 --attack_mode='fixed_generator' --poison_frac=0.3 --malicious_style=mixed  \
-            --attack_start_round=5  --load_pretrained=True --pretrained_path='../data/saved_models/tiny_64_pretrain/tiny-resnet.epoch_20' \
+            --attack_start_round=3  --load_pretrained=True --pretrained_path='../data/saved_models/tiny_64_pretrain/tiny-resnet.epoch_20' \
             --storing_dir='./perturbation_init_norm_seperate_vector_4_mali/tiny_perturbation_'$i --pattern_type="pixel" --save_checkpoint=True \
             --seperate_vector=True --save_trigger=True --norm_cap=$i --generator_lr=0.1
 done
